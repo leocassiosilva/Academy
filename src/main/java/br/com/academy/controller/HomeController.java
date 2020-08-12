2d63b06cd25ff0a36b32d804f7bc4dfe4676d099
leocassiosilva <leocassiosilva1234@gmail.com>
2020-08-12 08:11:56 -0300

    ModelAndView/home(index)

--- src/main/java/br/com/academy/controller/HomeController.java
+++ src/main/java/br/com/academy/controller/HomeController.java
@@ -2,12 +2,22 @@ package br.com.academy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-
+	
+	/*
 	@GetMapping("/home")
 	public String home() {
 		return "home";
+	}
+	*/
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home");
+		return mv;
 	}
 }
